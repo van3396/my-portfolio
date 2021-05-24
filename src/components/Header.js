e9339ad51a32d904cfdd85f4418d89265b27bd46
Qui <quivan3396@yahoo.com>
2021-05-24 13:12:18 -0400

    feat: add image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "../css-components/header.css";
 import a10 from "../images/a10.png";
+import qui from "../images/quiVanLogo.png";
 
 const Header = () => {
   return (
     <>
+      <img className="logo" src={qui} alt="" />
       <header className="header">
         <div>
-          <p className="fullName">
-            <strong className="fname">QUI </strong>VAN
-          </p>
           <p className="phrase">
             “Personal development is an unending challenge to see what you can
             become…”
@@ -18,11 +17,16 @@ const Header = () => {
             <i>– a Wiseman</i>
           </p>
           <div className="header_info">
-            <img className="me" src={a10} alt=""/>
-            <p className="header_about_me">If I'm not improving or studying new technologies, spending time with my wife and kids takes up my day.  My "me time", would be playing some poker.  Omaha 8/better is my main choice but I started off as a no-limit Hold'em player.</p>
+            <img className="me" src={a10} alt="" />
+            <p className="header_about_me">
+              If I'm not improving or studying new technologies, spending time
+              with my wife and kids takes up my day.  My "me time", would be
+              playing some poker.  Omaha 8/better is my main choice but I
+              started off as a no-limit Hold'em player.
+            </p>
           </div>
         </div>
-        <hr/>
+        <hr />
       </header>
     </>
   );
