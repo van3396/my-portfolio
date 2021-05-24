3a291b1834980f158da668a36dacfd1e300bd3d4
Qui <quivan3396@yahoo.com>
2021-05-24 14:03:28 -0400

    feat: delete app.css from App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import "../css-components/app.css"
-import Footer from './Footer'
-import Header from './Header';
-import Technologies from './Technologies'
-import Projects from './Projects'
+import React from "react";
 
+import Footer from "./Footer";
+import Header from "./Header";
+import Technologies from "./Technologies";
+import Projects from "./Projects";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Technologies/>
+      <Technologies />
       <Projects />
       <Footer />
     </div>
